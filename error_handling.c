43a7899539dff56a3d46fecb219a558a0a6872fd
Killian QUINTIN <killian.quintin@epitech.eu>
2023-11-10 11:13:35 +0100

    [FIX] error message in english

--- error_handling.c
+++ error_handling.c
@@ -11,9 +11,9 @@
 
 int error_opening(char *file_path, struct flags_list *flags, int i)
 {
-    write(2, "ls: impossible d'accéder à '", 30);
+    write(2, "ls: ", 4);
     write(2, file_path, my_strlen(file_path));
-    write(2, "' : Aucun fichier ou dossier de ce type", 39);
+    write(2, ": No such file or directory", 27);
     if (i < flags->total || flags->dir_name_ind != 0 ||
     flags->file_name_ind != 0)
         my_putchar('\n');
